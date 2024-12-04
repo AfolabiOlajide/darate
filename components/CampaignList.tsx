f0b70097645a4a3dcae497f5bd8fb2d0ba915be6
Afolabi Olajide Samuel <samolajide20@gmail.com>
2024-12-04 18:50:35 +0100

    added tag

--- components/CampaignList.tsx
+++ components/CampaignList.tsx
@@ -8,11 +8,14 @@ const CampaignList = ({ campaigns }: { campaigns: any[] }) => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-[2rem]">
             {campaigns.map((campaign: any) => {
+                const random4 = Math.floor(Math.random() * 4) + 1;
+                const random3 = Math.floor(Math.random() * 3) + 1;
+                const imgUrl = campaign.category === "nature" ? `/nature/${random4}.jpg` : campaign.category === "health" ? `/health/${random4}.jpg` : campaign.category === "education" ? `/edu/${random4}.jpg` : `/tech/${random3}.jpg`;
                 return (
                     <Link href={`/campaigns/${campaign.id}`} key={campaign.id}>
                         <BackgroundGradient className="rounded-[22px] p-[1rem] bg-zinc-900 cursor-pointer">
                             <Image
-                                src={`/nature/1.jpg`}
+                                src={imgUrl}
                                 alt="nature"
                                 height="400"
                                 width="400"
@@ -21,15 +24,11 @@ const CampaignList = ({ campaigns }: { campaigns: any[] }) => {
                             <p className="text-base font-bold sm:text-xl mt-4 mb-2 text-neutral-00">
                                 {campaign.title}
                             </p>
-
                             <p className="text-sm text-neutral-400">
                                 {campaign.description}
                             </p>
-                            <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 mt-4 text-xs font-bold bg-zinc-800">
-                                <span>Buy now </span>
-                                <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-                                    $100
-                                </span>
+                            <button className={`rounded-full px-4 py-1 flex items-center space-x-1 mt-4 text-xs ${ campaign.category === "nature" ? "nature" : campaign.category === "health" ? "health" : campaign.category === "education" ? "education" : campaign.category === "tech" ? "tech" : "other" }`}>
+                                <span>{campaign.category}</span>
                             </button>
                         </BackgroundGradient>
                     </Link>
