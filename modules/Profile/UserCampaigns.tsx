355691d900e967f1d9c6c70850b4c43490814c77
Afolabi Olajide Samuel <samolajide20@gmail.com>
2024-12-07 16:24:23 +0100

    setup user campaigns

--- modules/Profile/UserCampaigns.tsx
+++ modules/Profile/UserCampaigns.tsx
@@ -1,9 +1,21 @@
 "use client";
+import SkelentonLoadingList from "@/components/SkelentonLoadingList";
+import useContract from "@/hooks/useContract";
 import React from "react";
-import { useActiveAccount } from "thirdweb/react";
+import { useActiveAccount, useReadContract } from "thirdweb/react";
 
 const UserCampaigns = () => {
     const account = useActiveAccount();
+    const { contract } = useContract({
+        address: process.env.NEXT_PUBLIC_DARATE_CONTRACT_ADDRESS as string,
+    });
+    const { data, isPending } = useReadContract({
+        contract,
+        method: "function getUserCampaigns() view returns ((address campaignAddress, address owner, string title, uint256 creationTime)[])",
+        params: [],
+    });
+
+    console.log(data)
 
     return (
         <div>
@@ -14,6 +26,16 @@ const UserCampaigns = () => {
                     </p>
                 </div>
             )}
+            {account && !isPending && data?.length === 0 && (
+                <div className="flex items-center justify-center h-[50vh]">
+                    <p className="text-brand text-2xl font-bold">
+                        You have no Campaigns
+                    </p>
+                </div>
+            )}
+            {
+                account && isPending && <SkelentonLoadingList withImage />
+            }
         </div>
     );
 };
