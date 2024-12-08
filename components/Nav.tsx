1c3be742695f4d6792ff6cb2211048dc1a6a002c
Afolabi Olajide Samuel <samolajide20@gmail.com>
2024-12-08 13:19:40 +0100

    made darate logo a link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,17 +1,21 @@
-import { client } from '@/lib/constants';
-import React from 'react'
+'use client';
+import { client } from "@/lib/constants";
+import Link from "next/link";
+import React from "react";
 import { ConnectButton } from "thirdweb/react";
 
 const Nav = () => {
     return (
-        <nav className='cont py-[2rem] flex items-center justify-between'>
+        <nav className="cont py-[2rem] flex items-center justify-between">
             <div className="logo-container">
-                <span className='logo'>Darate</span>
+                <Link href="/">
+                    <span className="logo">Darate</span>
+                </Link>
             </div>
 
             <ConnectButton client={client} />
         </nav>
-    )
-}
+    );
+};
 
-export default Nav
+export default Nav;
